10d6eaf134f454cfeeafe25a21599cf9de73eba5
kirkabbott1 <kirkabbott1@gmail.com>
2021-01-27 00:58:10 -0500

    connects server to mongo

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -24,7 +24,6 @@ const orderSchema = mongoose.Schema(
       address: { type: String, required: true },
       city: { type: String, required: true },
       postalCode: { type: String, required: true },
-      state: { type: String, required: true },
       country: { type: String, required: true },
     },
     paymentMethod: {
@@ -35,11 +34,7 @@ const orderSchema = mongoose.Schema(
       id: { type: String },
       status: { type: String },
       update_time: { type: String },
-      email_address: true,
-    },
-    paymentMethod: {
-      type: String,
-      required: true,
+      email_address: { type: String },
     },
     taxPrice: {
       type: Number,
