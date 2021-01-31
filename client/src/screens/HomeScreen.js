e675a23dbecb5d86cfc2956f3f23128c724d94c7
kirkabbott1 <kirkabbott1@gmail.com>
2021-01-30 23:59:09 -0500

    rename folder

---
+++ client/src/screens/HomeScreen.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import { listProducts } from "../actions/productActions.js";
+
+const HomeScreen = () => {
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  console.log(products);
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <h1> Latest products</h1>
+      {loading ? (
+        <h2>loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => {
+            return (
+              <Col key={product._id} sm={12} md={6} lg={4}>
+                <Product product={product} />
+              </Col>
+            );
+          })}
+        </Row>
+      )}
+    </>
+  );
+};
+
+export default HomeScreen;
