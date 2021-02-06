5bebd7cc677e42dc0cd2b649dbbeaf5cd56147b3
kirkabbott1 <kirkabbott1@gmail.com>
2021-02-06 18:12:43 -0500

    add cart functionality

---
+++ client/src/screens/CartScreen.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+  ListGroupItem,
+} from "react-bootstrap";
+import { addToCart } from "../actions/cartActions";
+
+const CartScreen = ({ match, location, history }) => {
+  const productId = match.params.id;
+
+  const qty = location.search ? Number(location.search.split("=")[1]) : 1;
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1> Shopping Cart</h1>
+
+        {cartItems.length === 0 ? (
+          <Message>
+            Cart is empty <Link to='/'>Back to Home</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'></ListGroup>
+        )}
+      </Col>
+      <Col></Col>
+    </Row>
+  );
+};
+
+export default CartScreen;
--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -15,8 +15,8 @@ import { listProductDetails } from "../actions/productActions";
 import Loader from "../components/loader";
 import Message from "../components/Message";
 
-const ProductScreen = ({ match }) => {
-  const [qty, setQty] = useState(0);
+const ProductScreen = ({ history, match }) => {
+  const [qty, setQty] = useState(1);
   const dispatch = useDispatch();
 
   const productDetails = useSelector((state) => state.productDetails);
@@ -25,6 +25,10 @@ const ProductScreen = ({ match }) => {
   useEffect(() => {
     dispatch(listProductDetails(match.params.id));
   }, [dispatch, match]);
+
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`);
+  };
 
   return (
     <>
@@ -101,6 +105,7 @@ const ProductScreen = ({ match }) => {
 
                 <ListGroup.Item>
                   <Button
+                    onClick={addToCartHandler}
                     className='btn-block'
                     type='Button'
                     disabled={product.countInStock === 0}
